201a2d4e341954fe3aa4b9943907fbc3697440b3
mdao01 <md.almeidaoliveira@gmail.com>
2022-01-25 16:24:12 -0300

    first commit

---
+++ src/main/java/com/mdalmeidaoliveira/herdcontrol/HerdControlApplication.java
@@ -0,0 +1,13 @@
+package com.mdalmeidaoliveira.herdcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerdControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerdControlApplication.class, args);
+	}
+
+}
